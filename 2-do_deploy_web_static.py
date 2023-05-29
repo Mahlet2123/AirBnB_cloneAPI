3211661303d55ff8720f2cea1b1975624c3eb3c6
mahlet <akotet2123@gmail.com>
2023-05-29 02:29:10 -0700

    AirBnB_clone_v3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,47 +1,30 @@
 #!/usr/bin/python3
-""" Fabric script (based on the file 1-pack_web_static.py)
-that distributes an archive to your web servers,
-using the function do_deploy """
-from fabric.network import ssh_config
-import os
-from fabric.api import env, put, run
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
 
-
-env.hosts = ["34.207.222.16", "52.207.125.221"]
-env.user = "ubuntu"
-env.key_filename = "/root/.ssh/id_rsa"
-env.use_ssh_config = True
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['142.44.167.228', '144.217.246.195']
 
 
 def do_deploy(archive_path):
-    """
-    distributes an archive to your web servers
-    """
-    if os.path.exists(archive_path) is False:
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
     try:
-        put(archive_path, "/tmp/")
-        _filename = archive_path.split("/")[-1]
-        filename = _filename.split(".")[0]
-        run("mkdir -p /data/web_static/releases/{}".format(filename))
-        run(
-            "tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
-                _filename, filename
-            )
-        )
-        run("rm /tmp/{}".format(_filename))
-        run(
-            "mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".format(
-                filename, filename
-            )
-        )
-        run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
-        run("rm -rf /data/web_static/current")
-        run(
-            "ln -s /data/web_static/releases/{}".format(filename)
-            + " /data/web_static/current"
-        )
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except (IOError, OSError) as e:
+    except:
         return False
