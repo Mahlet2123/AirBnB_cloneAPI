3f8af7661d8aae501e02b66e004c34396b17f094
mahlet <akotet2123@gmail.com>
2023-05-30 04:55:17 -0700

    PEP fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,18 +12,21 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def remove_session(exception):
-    """ a Flask teardown function.
-    It is executed after each request to the Flask application. """
+    """a Flask teardown function.
+    It is executed after each request to the Flask application."""
     storage.close()
+
 
 @app_views.errorhandler(404)
 def not_found(error):
-    """ Handler for 404 errors """
+    """Handler for 404 errors"""
     return make_response(jsonify({"error": "Not found"}), 404)
 
+
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', '5000')
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
     app.run(host=host, port=port, threaded=True)
